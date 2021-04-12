cf91318cd29eed8a2b683b71e9aa093f010038c1
verybon <verybon@users.noreply.github.com>
2021-04-12 18:30:47 +0900

    Update ui-common.js

--- ui-common.js
+++ ui-common.js
@@ -1,5 +1,7 @@
-/* tab title */
+
 (function(){
+
+     /* tab title */
      $( ".tab-title a ").on( "click", function(e){
           e.preventDefault();
           var $t = $( this ),
@@ -8,4 +10,28 @@
           $p.find( "li" ).removeClass( "on" );
           $t.closest( "li" ).addClass( "on" );
      });
+
+     /* nav - show */
+     $( ".btn-show-nav" ).on( "click", function(e){
+          $( ".nav" ).addClass( "on" );
+     });
+     /* nav - close */
+     $( ".nav-btn-close" ).on( "click", function(e){
+          $( ".nav" ).removeClass( "on" );
+     });
+     /* nav - depth1 */
+     $( ".nav-depth1 > li > a" ).on( "click", function(e){
+          e.preventDefault();
+          $( ".nav-depth1 > li" ).removeClass( "on" );
+          $( this ).closest( "li" ).addClass( "on" );
+     });
+     /* nav - depth2 */
+     $( ".nav-depth2 > li.has-depth > a" ).on( "click", function(e){
+          var $target = $( this ).closest( ".has-depth" );
+          e.preventDefault();
+          $target.hasClass( "on" ) ? $target.removeClass( "on" ) : $target.addClass( "on" );
+     });
+
+     // btn-show-nav
+
 })();
